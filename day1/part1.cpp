d3a28b9c37e23ddd9f5b3a8cc4a52b18c730621c
Marcel <marcelvarga201@gmail.com>
2020-12-01 11:59:41 +0000

    day1

--- day1/part1.cpp
+++ day1/part1.cpp
@@ -3,18 +3,16 @@
 using namespace std;
 ifstream fi("in.in");
 int main(){
-    int i=0;
-    int a[1001];
-    while(fi>>a[i++]);
-    int n = i-1;
-    cout<<n<<"\n\n";
+    int n=0, a[1001];;
+    while(fi>>a[n++]);
+    n--;
     bool ok = 0;
     for(int i = 0; i < n - 1 and !ok; i++)
-        for(int j = i+1; j < n and !ok; j++){
+        for(int j = i+1; j < n and !ok; j++)
             if(a[i] + a[j] == 2020){
                 ok = 1;
-                cout<<i<<" "<<j<<" "<<a[i]<<" "<<a[j]<<" "<<a[i]+a[j]<<" product is "<<a[i]*a[j]<<'\n';
+                cout<<"Product is "<<a[i]*a[j]<<'\n';
             }
-        }
+        
     return 0;
 }
