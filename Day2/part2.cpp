7d5a46e541962aa33169b9e20991c6965e6d817c
Marcel <marcelvarga201@gmail.com>
2020-12-06 16:44:48 +0000

    Day 2

---
+++ Day2/part2.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+ifstream fi("in.in");
+int main(){
+    int n=0, a[1001];
+    while(fi>>a[n++]);
+    n--;
+    bool ok = 0;
+    for(int i = 0; i < n - 2 and !ok; i++)
+        for(int j = i+1; j < n - 1 and !ok; j++)
+            for(int k = j + 1; k < n and !ok; k++)
+            if(a[i] + a[j] + a[k] == 2020){
+                ok = 1;
+                cout<<"Product is "<<a[i] * a[j] * a[k]<<'\n';
+            }
+	fi.close();        
+    return 0;
+}
